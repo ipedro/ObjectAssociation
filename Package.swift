a3388ec96be7ba581612696edbd0858b827b0165
Pedro Almeida <ip4dro@gmail.com>
2021-05-05 11:35:15 +0200

    Fix platform requirement

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ObjectAssociation",
-    platforms: [.iOS(.v10), .macOS(.v10_15), .tvOS(SupportedPlatform.TVOSVersion.v10)],
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
